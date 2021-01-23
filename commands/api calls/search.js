01d6cd265ca8fbe59a6aa8f154c4890f046257e8
Malachi Brooks <malachi.evan@icloud.com>
2021-01-22 22:21:52 -0700

    Search added

---
+++ commands/api calls/search.js
@@ -0,0 +1,70 @@
+const { Command } = require("discord.js-commando");
+const axios = require("axios");
+const moment = require("moment");
+const config = require("./../../config.json");
+module.exports = class NAME extends Command {
+  constructor(client) {
+    super(client, {
+      name: "search",
+      group: "api calls",
+      aliases: ["image", "images", "lookup", "photo", "photos"],
+      memberName: "search",
+      description: "search the NASA image library for a image",
+      examples: [`${config.prefix}search <search term> [page number]`],
+      guildOnly: false,
+      ownerOnly: false,
+      throttling: {
+        usages: 3,
+        duration: 10,
+      },
+      args: [
+        {
+          key: "search_terms",
+          prompt: "Please choose a search term to look for",
+          type: "string",
+        },
+        {
+          key: "page_number",
+          prompt: "Please choose a page number to look for",
+          type: "integer",
+          default: "1",
+        },
+      ],
+    });
+  }
+  run(message, { type, sol, page_number }) {
+    search_terms = search_term.replace(/\s/g, "%20");
+    date = moment().utcOffset(-12).format("YYYY-M-D");
+
+    axios
+      .get("https://images-api.nasa.gov/search?q=" + search_term)
+      .then((res) => {
+        if (res.data.collection.metadata.total_hits == 0) {
+          message.reply("that search term found no results");
+          return;
+        }
+        img = res.data.collection.items[1].links[0].href.replace(/\s/g, "%20");
+        data = res.data.collection.items[1].data[0];
+
+        message.channel.send({
+          embed: {
+            title: data.title,
+            url: img,
+            description: `**Taken on:** ${data.date_created
+              .slice(0, 10)
+              .split(/ +/)}\n${data.description}`,
+            color: "#5A2017",
+            image: {
+              url: img,
+            },
+          },
+        });
+      })
+      .catch(function (error) {
+        console.log(error.stack);
+        message.say(
+          `An error occurred while running the command: ${error}\nFor help solving this problem please join are support server: ${invite}`
+        );
+      });
+  }
+};
