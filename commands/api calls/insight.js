828514641a539134481e5bea94f430a1875cc8f8
Malachi Brooks <malachi.evan@icloud.com>
2021-01-24 12:16:18 -0700

    Insight Works Now:tm:

--- commands/api calls/insight.js
+++ commands/api calls/insight.js
@@ -1,4 +1,6 @@
 const { Command } = require("discord.js-commando");
+const { RichEmbed } = require("discord.js");
+
 const axios = require("axios");
 const moment = require("moment");
 const config = require("./../../config.json");
@@ -58,20 +60,133 @@ module.exports = class Insight extends Command {
           `https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json`
         )
         .then((res) => {
-          console.log(res.data);
           let sol_keys = res.data.sol_keys;
+          let array_length = res.data.sol_keys.length;
+          //sol 1 data checks
+          let sol1 = sol_keys[array_length - 1];
+          let sol1_data = res.data[Object.keys(res.data)[array_length - 1]];
+          //check to see if temp data available
+          if (!sol1_data.AT.av) {
+            var sol1_av_temp = "NA";
+          } else var sol1_av_temp = sol1_data.AT.av + "°C";
+          if (!sol1_data.AT.mn) {
+            var sol1_mn_temp = "NA";
+          } else var sol1_mn_temp = sol1_data.AT.mn + "°C";
+          if (!sol1_data.AT.mx) {
+            var sol1_mx_temp = "NA";
+          } else var sol1_mx_temp = sol1_data.AT.mx + "°C";
+          //checks for wind data
+          if (!sol1_data.HWS.av) {
+            var sol1_av_wind = "NA";
+          } else var sol1_av_wind = sol1_data.HWS.av + "m/s";
+          if (!sol1_data.HWS.mn) {
+            var sol1_mn_wind = "NA";
+          } else var sol1_mn_wind = sol1_data.HWS.mn + "m/s";
+          if (!sol1_data.HWS.mx) {
+            var sol1_mx_wind = "NA";
+          } else var sol1_mx_wind = sol1_data.HWS.mx + "m/s";
+          //checks pr data
+          if (!sol1_data.PRE.av) {
+            var sol1_av_pr = "NA";
+          } else var sol1_av_pr = sol1_data.PRE.av + "Pa";
+          if (!sol1_data.PRE.mn) {
+            var sol1_mn_pr = "NA";
+          } else var sol1_mn_pr = sol1_data.PRE.mn + "Pa";
+          if (!sol1_data.PRE.mx) {
+            var sol1_mx_pr = "NA";
+          } else var sol1_mx_pr = sol1_data.PRE.mx + "Pa";
+          let sol2 = sol_keys[array_length - 1];
+          let sol2_data = res.data[Object.keys(res.data)[array_length - 1]];
+          //check to see if temp data available
+          if (!sol2_data.AT.av) {
+            var sol2_av_temp = "NA";
+          } else var sol2_av_temp = sol2_data.AT.av + "°C";
+          if (!sol2_data.AT.mn) {
+            var sol2_mn_temp = "NA";
+          } else var sol2_mn_temp = sol2_data.AT.mn + "°C";
+          if (!sol2_data.AT.mx) {
+            var sol2_mx_temp = "NA";
+          } else var sol2_mx_temp = sol2_data.AT.mx + "°C";
+          //checks for wind data
+          if (!sol2_data.HWS.av) {
+            var sol2_av_wind = "NA";
+          } else var sol2_av_wind = sol2_data.HWS.av + "m/s";
+          if (!sol2_data.HWS.mn) {
+            var sol2_mn_wind = "NA";
+          } else var sol2_mn_wind = sol2_data.HWS.mn + "m/s";
+          if (!sol2_data.HWS.mx) {
+            var sol2_mx_wind = "NA";
+          } else var sol2_mx_wind = sol2_data.HWS.mx + "m/s";
+          //checks pr data
+          if (!sol2_data.PRE.av) {
+            var sol2_av_pr = "NA";
+          } else var sol2_av_pr = sol2_data.PRE.av + "Pa";
+          if (!sol2_data.PRE.mn) {
+            var sol2_mn_pr = "NA";
+          } else var sol2_mn_pr = sol2_data.PRE.mn + "Pa";
+          if (!sol2_data.PRE.mx) {
+            var sol2_mx_pr = "NA";
+          } else var sol2_mx_pr = sol2_data.PRE.mx + "Pa";
+          let sol3 = sol_keys[array_length - 1];
+          let sol3_data = res.data[Object.keys(res.data)[array_length - 1]];
+          //check to see if temp data available
+          if (!sol3_data.AT.av) {
+            var sol3_av_temp = "NA";
+          } else var sol3_av_temp = sol3_data.AT.av + "°C";
+          if (!sol3_data.AT.mn) {
+            var sol3_mn_temp = "NA";
+          } else var sol3_mn_temp = sol3_data.AT.mn + "°C";
+          if (!sol3_data.AT.mx) {
+            var sol3_mx_temp = "NA";
+          } else var sol3_mx_temp = sol3_data.AT.mx + "°C";
+          //checks for wind data
+          if (!sol3_data.HWS.av) {
+            var sol3_av_wind = "NA";
+          } else var sol3_av_wind = sol3_data.HWS.av + "m/s";
+          if (!sol3_data.HWS.mn) {
+            var sol3_mn_wind = "NA";
+          } else var sol3_mn_wind = sol3_data.HWS.mn + "m/s";
+          if (!sol3_data.HWS.mx) {
+            var sol3_mx_wind = "NA";
+          } else var sol3_mx_wind = sol3_data.HWS.mx + "m/s";
+          //checks pr data
+          if (!sol3_data.PRE.av) {
+            var sol3_av_pr = "NA";
+          } else var sol3_av_pr = sol3_data.PRE.av + "Pa";
+          if (!sol3_data.PRE.mn) {
+            var sol3_mn_pr = "NA";
+          } else var sol3_mn_pr = sol3_data.PRE.mn + "Pa";
+          if (!sol3_data.PRE.mx) {
+            var sol3_mx_pr = "NA";
+          } else var sol3_mx_pr = sol3_data.PRE.mx + "Pa";
 
-          message.embed({
-            title: "",
-            url: "",
-            description: ``,
-            color: "#5A2017",
-            image: {
-              url: "",
-            },
-            footer: {
-              text: "Credit: NASA/JPL-Caltech",
-              icon_url: "",
+          message.channel.send({
+            embed: {
+              title: "Whether on mars for the last 3 days",
+              url: "https://mars.nasa.gov/insight/weather/",
+              description: ``,
+              fields: [
+                {
+                  name: `Weather From Sol: ${sol1}`,
+                  value: `**Average Temp:** ${sol1_av_temp} **Min:** ${sol1_mn_temp} **Max:** ${sol1_mx_temp}\n**Average Wind Speed:** ${sol1_av_wind} **Min:** ${sol1_mn_wind} **Max:** ${sol1_mx_wind}\n**Average Air Pressure :** ${sol1_av_pr} **Min:** ${sol1_mn_pr} **Max:** ${sol1_mx_pr}`,
+                },
+                {
+                  name: `Weather From Sol: ${sol2}`,
+                  value: `**Average Temp:** ${sol2_av_temp} **Min:** ${sol2_mn_temp} **Max:** ${sol2_mx_temp}\n**Average Wind Speed:** ${sol2_av_wind} **Min:** ${sol2_mn_wind} **Max:** ${sol1_mx_wind}\n**Average Air Pressure :** ${sol2_av_pr} **Min:** ${sol2_mn_pr} **Max:** ${sol2_mx_pr}`,
+                },
+                {
+                  name: `Weather From Sol: ${sol3}`,
+                  value: `**Average Temp:** ${sol3_av_temp} **Min:** ${sol3_mn_temp} **Max:** ${sol3_mx_temp}\n**Average Wind Speed:** ${sol3_av_wind} **Min:** ${sol3_mn_wind} **Max:** ${sol3_mx_wind}\n**Average Air Pressure :** ${sol3_av_pr} **Min:** ${sol3_mn_pr} **Max:** ${sol3_mx_pr}`,
+                },
+              ],
+              color: "#5A2017",
+              image: {
+                url: "",
+              },
+              footer: {
+                text: "Credit: NASA/JPL-Caltech",
+                icon_url: "",
+              },
             },
           });
         })
