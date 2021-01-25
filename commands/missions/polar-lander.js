5ed21df4ce9c702902495194ee0495cb1b7e93fc
Malachi Brooks <malachi.evan@icloud.com>
2021-01-24 18:43:01 -0700

    Mars Polar Lander/Deep Space 2

--- commands/missions/polar-lander.js
+++ commands/missions/polar-lander.js
@@ -1,18 +1,39 @@
-module.exports = {
-  name: "polar-lander",
-  description: "another mission",
-  execute(message, args) {
-    message.channel.send({
-      embed: {
-        title: "Mars Polar Lander/Deep Space 2",
-        description:
-          "Launched on January 3, 1999\nLaunched from Cape Canaveral Air Force Station, Florida\nMission Failed, lost on arival(December 3, 1999)\nMore info at:\nhttps://mars.nasa.gov/mars-exploration/missions/polar-lander/",
-        color: "#5A2017",
-        image: {
-          url:
-            "https://mars.nasa.gov/system/content_pages/main_images/381_mpl_shadow.jpg",
-        },
+const { Command } = require("discord.js-commando");
+const moment = require("moment");
+const config = require("./../../config.json");
+module.exports = class NAME extends Command {
+  constructor(client) {
+    super(client, {
+      name: "polar-lander",
+      group: "",
+      aliases: ["polar lander"],
+      memberName: "polar-lander",
+      description:
+        "Find Information on the Mars Polar Lander/Deep Space 2 mission",
+      examples: [`${config.prefix}`],
+      guildOnly: false,
+      ownerOnly: false,
+      throttling: {
+        usages: 2,
+        duration: 1,
       },
     });
-  },
+  }
+  run(message) {
+    message.embed({
+      title: "Mars Polar Lander/Deep Space 2",
+      url: "https://mars.nasa.gov/mars-exploration/missions/polar-lander/",
+      description:
+        "Launched on January 3, 1999\nLaunched from Cape Canaveral Air Force Station, Florida\nMission Failed, lost on arival(December 3, 1999)\nMore info at:\nhttps://mars.nasa.gov/mars-exploration/missions/polar-lander/",
+      color: "#5A2017",
+      image: {
+        url:
+          "https://mars.nasa.gov/system/content_pages/main_images/381_mpl_shadow.jpg",
+      },
+      footer: {
+        text: "Photo Credit: NASA/JPL-Caltech",
+        icon_url: "",
+      },
+    });
+  }
 };
