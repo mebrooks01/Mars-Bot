04ec2992928d6ad3b961caaaba17e0cb323aa8dc
Malachi Brooks <malachi.evan@icloud.com>
2021-02-11 22:01:28 -0700

    Update load.js

--- load.js
+++ load.js
@@ -7,38 +7,19 @@ module.exports = {
     );
     console.log(chalk.bold.underline(`Discord API Connection Info`));
     console.log(
-      chalk.yellow(`Username: `) +
-        `${info.tag}\n` +
-        chalk.yellow(`ID: `) +
-        `${info.id}\nOn ${info.server} Server(s)\n` +
-        chalk.yellow(`Owner(s): `) +
-        `${config.user_id.owner}`
+      chalk`{yellow Username:} ${info.tag}\n{yellow ID:} ${info.id}\n{yellow Servers:} ${info.server}\n{yellow Owner(s):} ${config.user_id.owner}`
     );
     console.log(chalk.bold.underline(`Database Connection Info`));
     console.log(
-      chalk.yellow(`Host: `) +
-        `${config.mysql.host}\n` +
-        chalk.yellow(`User: `) +
-        `${config.mysql.user}\n` +
-        chalk.yellow(`Database Name: `) +
-        `${config.mysql.db}`
+      chalk`{yellow Host:} ${config.mysql.host}\n{yellow User:} ${config.mysql.user}\n{yellow Database Name:} ${config.mysql.db}`
     );
     console.log(chalk.bold.underline(`NASA API Connection Info`));
-    console.log(chalk.yellow(`API Key: `) + `${config.api_key}`);
+    console.log(chalk`{yellow API Key:} ${config.api_key}`);
     console.log(chalk.bold.underline(`Other Info`));
     console.log(
-      chalk.yellow(`Prefix: `) +
-        `${config.prefix}\n` +
-        chalk.yellow(`Invite: `) +
-        `${config.invite}\n` +
-        chalk.yellow(`Embed Color: `) +
-        `${config.embed_color}\n` +
-        chalk.yellow(`Main Server: `) +
-        `${config.server_id.main_server}\n` +
-        chalk.yellow(`Daily APOD Channel: `) +
-        `${config.channel_id.apod_for_main}\n` +
-        chalk.yellow(`Daily APOD: `) +
-        `${config.dpod}`
+      chalk`{yellow Prefix:} ${config.prefix}\n{yellow Invite:} ${config.invite}\n{yellow Embed Color:}` +
+        chalk.hex(config.embed_color)(` ${config.embed_color}`) +
+        chalk`\n{yellow Main Server:} ${config.server_id.main_server}\n{yellow Daily APOD Channel:} ${config.channel_id.apod_for_main}\n{yellow Daily APOD:} ${config.dpod}`
     );
     console.log(
       chalk.bold.blue(`----------------------------------------------------`)
