a3414d23adc0c9c12b4746d73bf08111c674bfa6
Malachi <Brooks>
2021-02-25 13:46:30 -0700

    api call added

--- commands/api calls/perseverance.js
+++ commands/api calls/perseverance.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const moment = require('moment')
 
 const config = require('$root/config.json')
+const mission = require('$root/mission.json')
 
 module.exports = class Perseverance extends Command {
   constructor(client) {
@@ -44,12 +45,46 @@ module.exports = class Perseverance extends Command {
   }
 
   run(message, { type, sol, result_number }) {
-    if (type === 'info') {
+    if (type === 'image') {
+      if (!sol)
+        return message.reply(
+          'Please choose a sol to look for\n`=curiosity image <sol> <result number>`',
+        )
+      if (!result_number)
+        return message.reply(
+          'Please choose a result number to look for\n`=curiosity image <sol> <result number>`',
+        )
+
       axios
         .get(
-          `https://api.nasa.gov/mars-photos/api/v1/manifests/perseverance?api_key=${config.api_key}`,
+          `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?sol=${sol}&api_key=${config.api_key}`,
         )
-        .then((res) => {})
+        .then((res) => {
+          if (!res.data.photos[result_number - 1]) {
+            return message.reply('No results found')
+          }
+          let img = res.data.photos[result_number - 1].img_src
+          let data = res.data.photos[result_number - 1]
+          let cam = res.data.photos[result_number - 1].camera
+          let rover = res.data.photos[result_number - 1].rover
+
+          message.channel.send({
+            embed: {
+              title: 'Photo from ' + rover.name + "'s from " + cam.full_name,
+              url: img,
+              description: `**Rover Name:** ${rover.name}\n**Mission Status:** ${rover.status}\n**Sol:** ${data.sol}\n**Date:** ${data.earth_date}\n**Camera Name:** ${cam.full_name} (${cam.name})\n**Photo ID:** ${data.id}`,
+              color: this.client.config.embed_color,
+              timestamp: new Date(),
+              image: {
+                url: img,
+              },
+              footer: {
+                text: 'Photo Credit: NASA/JPL-Caltech',
+                icon_url: '',
+              },
+            },
+          })
+        })
         .catch(function (error) {
           console.log(error.stack)
           message.reply(
@@ -58,5 +93,22 @@ module.exports = class Perseverance extends Command {
         })
       return
     }
+    message.embed({
+      title: 'Mars 2020 Perseverance Rover',
+      url: mission.url,
+      description: `**API data not available  yet**\n${
+        mission.perseverance.info
+      }\nMore Info at:\n${(mission.perseverance, url)}`,
+      color: config.embed_color,
+      timestamp: new Date(),
+      image: {
+        url: mission.perseverance.img,
+      },
+      footer: {
+        text: 'Credit: NASA/JPL-Caltech',
+        icon_url: '',
+      },
+    })
+    return
   }
 }
