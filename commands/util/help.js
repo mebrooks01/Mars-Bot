76174a213319ca079c80de16f094df55408d319f
Malachi <Brooks>
2021-04-17 14:24:09 -0600

    dm user

--- commands/util/help.js
+++ commands/util/help.js
@@ -83,16 +83,24 @@ module.exports = class Help extends Command {
       await fields.push({ name: `**__${group.name}__**`, value: cmdList })
     })
 
-    await message.embed({
-      title: `Mars Bot Help (Prefix: \`${message.guild.commandPrefix}\`)`,
-      url: 'https://github.com/mebrooks01/Mars-Bot/blob/main/README.md',
-      description:
-        "**Key**:\n⚠️ Guild Only\nArgument names enclosed in `[square brackets]` are optional. Ones enclosed in `<angle brackets>` are required. Ones enclosed in `['single quotes']` mean that you should type their name to toggle an option (instead of providing a value of your own). Ones separated by `<vertical | bars>` mean you can choose between one of them (this also applies to quoted arguments).\nPS Thx catte for the arguments explanation",
-      fields,
-      color: config.embed_color,
-      timestamp: new Date(),
-      thumbnail: { url: config.pfp }
-    })
+    try {
+      await message.author.send({
+        embed: {
+          title: `Mars Bot Help (Prefix: \`${message.guild.commandPrefix}\`)`,
+          url: 'https://github.com/mebrooks01/Mars-Bot/blob/main/README.md',
+          description:
+            "**Key**:\n⚠️ Guild Only\nArgument names enclosed in `[square brackets]` are optional. Ones enclosed in `<angle brackets>` are required. Ones enclosed in `['single quotes']` mean that you should type their name to toggle an option (instead of providing a value of your own). Ones separated by `<vertical | bars>` mean you can choose between one of them (this also applies to quoted arguments).\nPS Thx catte for the arguments explanation",
+          fields,
+          color: config.embed_color,
+          timestamp: new Date(),
+          thumbnail: { url: config.pfp }
+        }
+      })
+      await message.react('✅')
+    } catch (error) {
+      await message.reply('Could not send a DM to you\nYou probably have DMs off')
+      await message.react('❌')
+    }
     fields = []
   }
 }
